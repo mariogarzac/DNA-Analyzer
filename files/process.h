c5b89dd6e02ae9dc946214485a9a828f842bca64
Mario Garza Chapa <mariogarza@Marios-Air-2.lan>
2022-06-12 22:42:40 -0500

    Initial Commit

---
+++ files/process.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+void printHello(){
+    printf("HOLA\n");
+    printf("HOLA\n");
+    printf("HOLA\n");
+    printf("HOLA\n");
+    printf("HOLA\n");
+    printf("HOLA\n");
+}
